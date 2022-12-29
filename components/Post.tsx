68f4e8013a2a105f463dd460a07afaa948bfdcd3
Sanjay Soundarajan <sanjay.16@live.com>
2022-12-28 16:10:21 -0800

    initial: 🎉 first commit

---
+++ components/Post.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/no-children-prop */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import Router from 'next/router';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+export type PostProps = {
+  id: string;
+  title: string;
+  author: {
+    name: string;
+    email: string;
+  } | null;
+  content: string;
+  published: boolean;
+};
+
+const Post: React.FC<{ post: PostProps }> = ({ post }) => {
+  const authorName = post.author ? post.author.name : 'Unknown author';
+  return (
+    <div onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}>
+      <h2>{post.title}</h2>
+      <small>By {authorName}</small>
+      <ReactMarkdown children={post.content} />
+      <style jsx>
+        {`
+          div {
+            color: inherit;
+            padding: 2rem;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Post;
